3556a41ad8e7a75503add98548e75b7efef4aff7
Serafaker <77161010+Serafaker@users.noreply.github.com>
2021-01-23 21:20:13 +0300

    Homework1
    готово

--- dz.js
+++ dz.js
@@ -1,16 +1,19 @@
+'use strict';
 
+let pic ='';
+let name1 = prompt("Ваше имя?");
 
-let name = "Василий";
-
-let admin = name;
+let admin = name1;
 alert(admin);
 console.log(admin);
 
-let NumberOfPic = prompt("Введи номер картинки!");
+let NumberOfPic = prompt("Введи номер картинки!!");
 let FormatOfPic = prompt("Введи формат картинки .png или .jpg");
 
-if (NumberOfPic !=NaN && FormatOfPic !=NaN ){
+if (NumberOfPic <=11 && FormatOfPic === 'png' || FormatOfPic === 'jpg'){
 
-document.getElementById('pic').src = `images/${NumberOfPic}${FormatOfPic}`;
-console.log(pic);
+    document.getElementById("pic").src = `images/${NumberOfPic}.${FormatOfPic}`;
+
+} else  {
+    alert('Ошибка, номер от 0-11 и поддерживаются форматы png или jpg');
 }
